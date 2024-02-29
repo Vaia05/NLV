d256d3ac89d8f97a665ab303ed9caf7b0931750e
Jens <157013039+JensMartensen@users.noreply.github.com>
2024-02-29 13:07:51 +0100

    ah

--- bron.php
+++ bron.php
@@ -1,12 +1,108 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+
+session_start();
+if (isset($_POST["info"]))
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bronvermelding</title>
+    <title>Homepage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        body{
+            background-color: #818181;
+        }
+        #sidebar {
+            height: 100%;
+            width: 0;
+            position: fixed;
+            z-index: 1;
+            top: 0;
+            left: 0;
+            background-color: #111;
+            overflow-x: hidden;
+            transition: 0.5s;
+            padding-top: 60px;
+        }
+
+        #sidebar a {
+            padding: 10px 15px;
+            text-decoration: none;
+            font-size: 25px;
+            color: #818181;
+            display: block;
+            transition: 0.3s;
+        }
+
+        #sidebar a:hover {
+            color: #f1f1f1;
+        }
+
+        #sidebar .closebtn {
+            position: absolute;
+            top: 0;
+            right: 25px;
+            font-size: 36px;
+            margin-left: 50px;
+        }
+
+        #main-content {
+            transition: margin-left .5s;
+            padding: 16px;
+        }
+    </style>
 </head>
+<nav>
+
+</nav>
 <body>
-    <h1>Bronvermelding<h1>
-    <h2>alle links van de bronnen komen hier te staan</h2>
+    <div id="sidebar">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+
+        <a href="standpunten.php">Standpunten</a>
+        <a href="bron.php">Bronvermelding</a>
+        <a href="contact.php">Contact</a>
+    </div>
+    <div id="main-content">
+        <span class="header" style="font-size:30px;cursor:pointer" onclick="openNav()">☰</span>
+        <h1 style="text-align: center;">Bronvermelding</h1>
+        
+        <div class="beschrijving-sm">
+            <div class="text-white" style="height: 480px; margin: 10px; text-align: center;">
+                <p><img src="logo.png"></p>
+                <p>Inleiding o Beschrijving van de politieke stroming (minimaal 200 woorden)
+o Beschrijving van jullie eigen politieke partij (minimaal 150 woorden)</p>
+
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div style="text-align: center;">
+            <p>©2024 NLV</p>
+        </div>
+    </footer>
+
+    <script>
+        function openNav() {
+            document.getElementById("sidebar").style.width = "250px";
+            document.getElementById("main-content").style.marginLeft = "250px";
+        }
+
+        function closeNav() {
+            document.getElementById("sidebar").style.width = "0";
+            document.getElementById("main-content").style.marginLeft = "0";
+        }
+
+        window.onload = function() {
+            if (document.cookie.split(';').some((item) => item.trim().startsWith('sidebar_open='))) {
+                document.getElementById("sidebar").style.width = "250px";
+                document.getElementById("main-content").style.marginLeft = "250px";
+            }
+        };
+    </script>
 </body>
+
 </html>
+
